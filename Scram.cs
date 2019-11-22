6cf6b41cf571cc8250144d68864a1c6cbd724b65
nate contino <nathan.contino@10gen.com>
2019-11-22 16:05:00 -0500

    Create Scram.cs

---
+++ Scram.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.IO;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+// Tested using MongoDB.Driver 2.9.2 and netcoreapp 2.2
+
+namespace WorkingWithMongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            var connectionString = "mongodb+srv://admin:admin@balboaclusterm10-5sfvt.mongodb.net/test?retryWrites=true&w=majority";
+
+            var client = new MongoClient(connectionString);
+            
+            // just doing a quick read to verify the usability of this connection
+            var database = client.GetDatabase("testDB");
+            var collection = database.GetCollection<BsonDocument>("testCol");
+            
+            var docCount = collection.CountDocuments("{}");
+            Console.WriteLine(docCount);
+        }
+    }
+}
